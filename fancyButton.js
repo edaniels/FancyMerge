e6016efe48087bc40dc478a4b6ef65bcb5951b40
Eric Daniels <eric@erdaniels.com>
2015-03-04 22:00:14 -0500

    rely on page information for buttons initial state

--- fancyButton.js
+++ fancyButton.js
@@ -2,12 +2,30 @@ var NAME = "contentscript";
 
 window.addEventListener('load', main, false);
 
+function toggle(target, val) {
+    target.prop('disabled', !val);
+    if (val) {
+        target.addClass('primary');
+    } else {
+        target.removeClass('primary');
+    }
+}
+
 function main() {
-    var fancyMergeBtn = $('<button class="button merge-branch-action" disabled><span class="octicon octicon-git-merge"></span>\nFancyMerge</button>');
+
+    var show = !!$('.branch-action-state-clean, .branch-action-state-dirty').length;
+    if (!show) {
+        return;
+    }
+
+    var enabled = !!$('.branch-action-state-clean').length;
+    var fancyMergeBtn = 
+        $('<button class="button merge-branch-action" disabled><span class="octicon octicon-git-merge"></span>\nFancyMerge</button>');
     fancyMergeBtn.click(function() {
         console.log('sending message');
     });
-    $('.merge-message').prepend(fancyMergeBtn);
+    toggle(fancyMergeBtn, enabled);
+    $('.branch-action-state-clean .merge-message, .branch-action-state-dirty .merge-message').prepend(fancyMergeBtn);
 
     chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 
@@ -17,12 +35,12 @@ function main() {
         
         switch (request.action) {
             case 'canFancyMerge':
-                fancyMergeBtn.prop('disabled', !request.data);
-                if (request.data) {
-                    fancyMergeBtn.addClass('primary');
-                } else {
-                    fancyMergeBtn.removeClass('primary');
-                }
+                // fancyMergeBtn.prop('disabled', !request.data);
+                // if (request.data) {
+                //     fancyMergeBtn.addClass('primary');
+                // } else {
+                //     fancyMergeBtn.removeClass('primary');
+                // }
                 break;
         }
     });
